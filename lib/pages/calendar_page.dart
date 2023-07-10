d6bc0375b076741fb53d60839d264e7783199f88
EugeniusStrong <EugeniusStrong@gmail.com>
2023-07-10 14:40:31 +0500

    Created table calendar

--- lib/pages/calendar_page.dart
+++ lib/pages/calendar_page.dart
@@ -1,70 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class CalendarPage extends StatefulWidget {
-  const CalendarPage({super.key});
+  const CalendarPage({Key? key}) : super(key: key);
 
   @override
   State<CalendarPage> createState() => _CalendarPageState();
 }
 
 class _CalendarPageState extends State<CalendarPage> {
-  List<int> dayWeek =
-      List<int>.generate(35, (index) => index < 31 ? index + 1 : 0);
-  int _chooseNumber = 0;
+  CalendarFormat format = CalendarFormat.month;
+  DateTime _selectedDay = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white70,
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text('Month'),
         centerTitle: true,
       ),
-      body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: GridView.builder(
-                itemCount: 35,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 7),
-                itemBuilder: (BuildContext context, int index) {
-                  return GestureDetector(
-                    onTap: () {
-                      _tapped(index);
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.black54,
-                        ),
-                        color: dayWeek[index] == _chooseNumber
-                            ? Colors.green
-                            : Colors.transparent,
-                      ),
-                      child: Center(
-                        child: Text(
-                          dayWeek[index] != 0 ? dayWeek[index].toString() : '',
-                          style: const TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.w400),
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ],
+      body: TableCalendar(
+        focusedDay: _selectedDay,
+        calendarFormat: format,
+        firstDay: DateTime(1989),
+        lastDay: DateTime(2050),
+        startingDayOfWeek: StartingDayOfWeek.monday,
+        daysOfWeekVisible: true,
+        onDaySelected: (DateTime selectDay, _) {
+          setState(() {
+            _selectedDay = selectDay;
+          });
+        },
+        selectedDayPredicate: (DateTime date) {
+          return isSameDay(_selectedDay, date);
+        },
+        calendarStyle: CalendarStyle(
+          isTodayHighlighted: true,
+          todayDecoration: BoxDecoration(
+            color: Colors.blueGrey[600],
+            shape: BoxShape.circle,
+          ),
+          selectedDecoration: const BoxDecoration(
+            color: Colors.blue,
+            shape: BoxShape.circle,
+          ),
+          selectedTextStyle: const TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          ),
+        ),
+        headerStyle: const HeaderStyle(
+          formatButtonVisible: false,
+          titleCentered: true,
         ),
       ),
     );
-  }
-
-  void _tapped(int index) {
-    setState(() {
-      if (dayWeek[index] != 0) {
-        _chooseNumber = dayWeek[index];
-      }
-    });
   }
 }
